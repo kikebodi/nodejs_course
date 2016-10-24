9a99b16631f03127fa2fc0ac5110c57fb0456078
kikebodi <bodi.inf@gmail.com>
2016-10-24 18:09:00 +0200

    First example

---
+++ node-examples/simplerect.js
@@ -0,0 +1,32 @@
+//This is the first example in Node JS. Where we define the object functions all together.
+
+//This is the object functions (we will separate it latter in another file)
+var rect = {
+	perimeter: function (x, y) {
+           return (2*(x+y));
+	},
+	area: function (x, y) {
+            return (x*y);
+	}
+};
+
+//This is the method that uses the Object functions.
+function solveRect(l,b) {
+    console.log("Solving for rectangle with l = " + l + " and b = " + b);
+
+    if (l < 0 || b < 0) {
+        console.log("Rectangle dimensions should be greater than zero:  l = "
+               + l + ",  and b = " + b);
+    }
+    else {
+	console.log("The area of a rectangle of dimensions length = "
+               + l + " and breadth = " + b + " is " + rect.area(l,b));
+	console.log("The perimeter of a rectangle of dimensions length = "
+               + l + " and breadth = " + b + " is " + rect.perimeter(l,b));
+    }
+}
+
+//These are the calls for the method.
+solveRect(2,4);
+solveRect(3,5);
+solveRect(-3,5);
