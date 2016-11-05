693ebf60730fc47a228589a17b8c7e7ea21616f3
kikebodi <bodi.inf@gmail.com>
2016-11-05 15:33:25 +0100

    Exercise 11: Mongoose. Update schema

--- node-mongoose/server-2.js
+++ node-mongoose/server-2.js
@@ -16,34 +16,40 @@ db.once('open', function(){
 	//We are connected
 	console.log('Connection to MongoDB: OK');
 
-	//Create new dish...
-	var newDish = dishes({
+	//Here we create a new dish
+	dishes.create({
 		name: 'Coolpizza',
-		description: 'Test..'
-	});
-	//... and save the dish
-	newDish.save(function(err){
+		description: 'Test'
+	}, function(err,dish){
 		if(err){
 			throw err;
 		}else{
 			console.log('Dish created');
+			console.log(dish);
+			var id = dish.id;
+
+			//set a bit delay (3 seconds)
+			setTimeout(function(){
+				dishes.findByIdAndUpdate(id, {
+					$set: {
+						description: 'Updated Test'
+					}
+				},{
+					new:true
+				}).exec(function(err, dish){
+					if(err){
+						throw err;
+					}else{
+						console.log('Updated Dish');
+						console.log(dish);
+
+						db.collection('dishes').drop(function(){
+							db.close();
+						});
+					}
+				});
+			}, 3000);
 		}
-
-		//Get all the dishes 
-		dishes.find({}, function(err,listDishes){
-			if(err){
-				throw err;
-			}else{
-				//Print the object of all the users
-				console.log(listDishes);
-			}
-
-			//Close connections
-			db.collection('dishes').drop(function(){
-				db.close();
-			});
-		});
 	});
 
 });
-
