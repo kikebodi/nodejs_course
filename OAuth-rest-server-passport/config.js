a915402f1dd618713eebdac185fd7e1a4a15f4b1
Kike Bodí <bodi.inf@gmail.com>
2016-11-30 17:55:36 +0100

    Update config.js

--- OAuth-rest-server-passport/config.js
+++ OAuth-rest-server-passport/config.js
@@ -2,8 +2,8 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl' : 'mongodb://localhost:27017/conFusion',
     'facebook': {
-    	clientID: '263005637208652',
-    	clientSecret: '8132c752d2af0124d3335f6669b11355',
+    	clientID: 'xxxxxxxxxxxx',
+    	clientSecret: 'xxxxxxxxxxxxxxxxx',
     	callbackURL: 'https://localhost:3443/users/facebook/callback'
     }
 }
